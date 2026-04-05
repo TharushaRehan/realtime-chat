15b25128cadeb9b05ca3c635ffc0fbdfba2c59ad
TharushaRehan <tharushaperera088@gmail.com>
2026-04-05 10:53:56 +0530

    feat: implement chat room functionality with real-time messaging and authentication

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,6 +1,9 @@
 import { redis } from "@/lib/redis";
-import { Elysia, t } from "elysia";
+import { Elysia } from "elysia";
 import { nanoid } from "nanoid";
+import { authMiddleware } from "./auth";
+import z from "zod";
+import { Message, realtime } from "@/lib/realtime";
 
 const ROOM_TTL_SECONDS = 60 * 10; // 10 minutes
 
@@ -18,7 +21,49 @@ const rooms = new Elysia({ prefix: "/room" }).post("/create", async () => {
   return { room_id };
 });
 
-const app = new Elysia({ prefix: "/api" }).use(rooms);
+const messages = new Elysia({ prefix: "/messages" }).use(authMiddleware).post(
+  "/",
+  async ({ auth, body }) => {
+    const { sender, text } = body;
+    const { room_id, token, connected } = auth;
+
+    const room_exists = await redis.exists(`meta:${room_id}`);
+
+    if (!room_exists) {
+      throw new Error("Room does not exist");
+    }
+
+    const message: Message = {
+      id: nanoid(),
+      sender,
+      text,
+      timestamp: Date.now(),
+      room_id,
+    };
+
+    // add message to history
+    await redis.rpush(`messages:${room_id}`, { ...message, token });
+
+    await realtime.channel(room_id).emit("chat.message", message);
+
+    // housekeeping
+    const remaining_ttl = await redis.ttl(`meta:${room_id}`);
+    await redis.expire(`messages:${room_id}`, remaining_ttl);
+    await redis.expire("history:${room_id}", remaining_ttl);
+    await redis.expire(room_id, remaining_ttl);
+  },
+  {
+    query: z.object({
+      room_id: z.string(),
+    }),
+    body: z.object({
+      sender: z.string().max(100),
+      text: z.string().max(1000),
+    }),
+  },
+);
+
+const app = new Elysia({ prefix: "/api" }).use(rooms).use(messages);
 
 export type App = typeof app;
 
