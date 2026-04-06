adb38092de1f9615d19d711ed85fea866d333367
TharushaRehan <tharushaperera088@gmail.com>
2026-04-06 22:07:31 +0530

    feat: enhance room creation with creator info and improve UI components for better user experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Spinner } from "@/components/ui/spinner";
 import { useUsername } from "@/hooks/use-username";
 import { client } from "@/lib/client";
 import { useMutation } from "@tanstack/react-query";
@@ -22,9 +25,11 @@ function Lobby() {
   const destroyed = searchParams.get("destroyed") === "true";
   const error = searchParams.get("error");
 
-  const { mutate: createRoom } = useMutation({
+  const { mutate: createRoom, isPending } = useMutation({
     mutationFn: async () => {
-      const res = await client.room.create.post();
+      const res = await client.room.create.post({
+        created_by: username,
+      });
       if (res.status === 200) {
         router.push(`/room/${res.data?.room_id}`);
       }
@@ -71,21 +76,23 @@ function Lobby() {
         <div className="border border-zinc-800 bg-zinc-900/50 p-6 backdrop-blur-md">
           <div className="space-y-5">
             <div className="space-y-2">
-              <label className="flex items-center text-zinc-500">
+              <Label className="flex items-center text-zinc-500">
                 Your Identity
-              </label>
+              </Label>
               <div className="flex items-center gap-3">
                 <div className="flex-1 bg-zinc-950 border border-zinc-800 p-3 text-sm text-zinc-400 font-mono">
                   {username}
                 </div>
               </div>
             </div>
-            <button
+            <Button
               onClick={() => createRoom()}
-              className="w-full bg-zinc-100 text-black p-3 text-sm font-bold hover:bg-zinc-50 hover:text-black transition-colors mt-2 cursor-pointer disabled:opacity-50"
+              size={"lg"}
+              className="w-full"
+              disabled={isPending}
             >
-              CREATE SECURE ROOM
-            </button>
+              {isPending ? <Spinner /> : "Create New Room"}
+            </Button>
           </div>
         </div>
       </div>
